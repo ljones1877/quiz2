a321f4192dd2e07105fb208437211bd6dc3ee43e
ljones1877 <jlorin7@gmail.com>
2019-11-12 05:25:56 +0000

    Added Create Button, not linked

--- app/controllers/info_controller.rb
+++ app/controllers/info_controller.rb
@@ -4,6 +4,21 @@ class InfoController < ApplicationController
   end
 
   def new
-    @info = info.new
+    @info = Info.new
+  end
+
+  def create
+    Info.create(info_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def info_params
+    params.require(:info).permit(:name, :information)
+  end
+
+  def show
+    @info = Info.find
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root 'info#index'
+  resources :infos, only: [:new, :create, :index]
+  resources :info, only: [:new, :create, :index]
 end
