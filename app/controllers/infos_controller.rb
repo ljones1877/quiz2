a1143b6cf1972c1ae82b374cbd97704220114c8c
ljones1877 <jlorin7@gmail.com>
2019-11-17 21:30:42 +0000

    Quiz info page working

--- app/controllers/infos_controller.rb
+++ app/controllers/infos_controller.rb
@@ -4,15 +4,36 @@ class InfosController < ApplicationController
     @infos = Info.all
   end
 
+  def new
+    @info = Info.new
+  end
+
   def create
     current_user.infos.create(info_params)
     redirect_to root_path
   end
 
+
   def show
-    @infos = Info.find(params[:id])
+    @info = Info.find(params[:id])
   end
 
+  def edit
+    @info = Info.find(params[:id])
+  end
+
+  def update
+    @info = Info.find(params[:id])
+    @info.update_attributes(info_params)
+    redirect_to root_path
+  end
+
+  def destroy
+  @info = Info.find(params[:id])
+  @info.destroy
+  redirect_to root_path
+end
+ 
   private
 
   def info_params
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,4 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :infos
 
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
 end
